3d4f4f31e0f3863fa4552d83a1c10a4e9a8d876a
DESKTOP-DE46TUB\David <david.oliveira.barbosa@gmail.com>
2017-09-04 22:08:37 -0300

    Adicionar arquivos de projeto.

---
+++ TesteGraficos/TesteGraficos/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TesteGraficos
+{
+    public partial class MainPage : ContentPage
+    {
+        OxyExData Model { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Model = new OxyExData();
+            
+            BindingContext = Model;
+        }
+    }
+}
